efe7a1983224a5406caa4a23ed0653bd4f93a9b4
Robert Finedore <robertfinedore@gmail.com>
2023-02-16 17:47:42 -0600

    integrated Three.js, orbital and ply data

--- cranio_norm_app/static/skull_input.js
+++ cranio_norm_app/static/skull_input.js
@@ -5,47 +5,49 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.z = 5;
+camera.position.z = 1;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
 
 const geometryPoints = new THREE.BufferGeometry();
 
 var rawData = [];
+var positionPoints = [];
 
 var materialTest = new THREE.PointsMaterial({
-  size: 0.01,
-  vertexColors: true,
+  size: 0.1,
+  color: 0xff0000,
 });
+
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 $(document).ready(function () {
   rawData = JSON.parse(data);
-  console.log(typeof rawData);
-  console.log(rawData);
 
   document.body.appendChild(renderer.domElement);
+  processRawData();
   // scene.add(cube);
   animate();
 });
 
 function animate() {
+  controls.update();
   requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 }
 
 function processRawData() {
+  for (i = 0; i < rawData.length; i++) {
+    positionPoints.push(rawData[i][0], rawData[i][1], rawData[i][2]);
+  }
   geometryPoints.setAttribute(
     "position",
-    new THREE.Float32BufferAttribute(rawData, 3)
+    new THREE.Float32BufferAttribute(positionPoints, 3)
   );
   particles = new THREE.Points(geometryPoints, materialTest);
   scene.add(particles);
